75ea60ae7d2070d89f841c25d170e62b24e3d8a7
Andrew PeaR <minecraft145236@gmail.com>
2024-01-20 20:14:48 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import Categories from "./components/Categories";
+import Header from "./components/Header";
+import PizzaBlock from "./components/PizzaBlock";
+import Sort from "./components/Sort";
+
+import pizzas from "./assets/pizzas.json";
+
+import "./scss/app.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="wrapper">
+        <Header />
+        <div className="content">
+          <div className="container">
+            <div className="content__top">
+              <Categories />
+              <Sort />
+            </div>
+            <h2 className="content__title">Все пиццы</h2>
+            <div className="content__items">
+              {pizzas.map((pizza) => (
+                <PizzaBlock key={pizza.id} {...pizza} />
+                // <PizzaBlock
+                //   title={pizza.title}
+                //   price={pizza.price}
+                //   imageUrl={pizza.imageUrl}
+                //   sizes={pizza.sizes}
+                //   types={pizza.types}
+                // />
+              ))}
+
+              {/* <PizzaBlock title="Мексиканская" price="500" /> */}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
